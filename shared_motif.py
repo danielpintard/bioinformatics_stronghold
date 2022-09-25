15471a80eb7bf6d0357bb6f6b61d8e8daf07313f
danielpintard <danielpintard@yahoo.com>
2022-09-24 20:28:04 -0400

    latest  = new Date

--- shared_motif.py
+++ shared_motif.py
@@ -1,5 +1,5 @@
 # not outputting correct answer 
-data = open('/Users/danielpintard/Downloads/rosalind_lcsm (1).txt', 'r').read()
+data = open('/Users/danielpintard/Downloads/rosalind_lcsm (3).txt', 'r').read()
 # data = open('shared_motif_samdata.txt', 'r').read()
 
 if ">" in data: 
@@ -15,21 +15,21 @@ for i in data_array:
     i = data_array[data_array.index(i)][1]
     sequences.append(i)
 
-
     
 sorted_sequences = sorted(sequences, key=len)
 shrt_seq = sorted_sequences[0]
 comparing_seq = sorted_sequences[1:]
 motif = ''
-for i in range(len(sorted_sequences)):
+for i in range(len(shrt_seq)):
     for s in range(i, len(shrt_seq)): 
-        m = shrt_seq[i::s+1]
+        m = shrt_seq[i:s + 1]
         found = False
         for sequences in comparing_seq:
             if m in sequences:
                 found = True
-            else: found = False
-            break 
+            else: 
+                found = False
+                break 
         if found and len(m) > len(motif):
             motif = m 
         
