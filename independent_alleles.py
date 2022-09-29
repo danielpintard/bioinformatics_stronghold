3400ebade44da13e5d2d3acbb0622af204b4fc91
danielpintard <danielpintard@yahoo.com>
2022-09-29 18:33:24 -0400

    black-boxed code

---
+++ independent_alleles.py
@@ -0,0 +1,9 @@
+from scipy.special import binom
+
+def second_law(k, N):
+    def p(n, k):
+        return binom(2**k, n) * 0.25**n * 0.75**(2**k-n)
+    return 1 - sum(p(n, k) for n in range(N))
+
+print(second_law(2,1))
+
